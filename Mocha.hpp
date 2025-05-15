08ed89250e56c2e14a0cb7410cc492e35147d2f0
josealbertoruizr <josealbertoruizr@gmail.com>
2025-05-15 17:23:56 -0600

    solved an error

--- Mocha.hpp
+++ Mocha.hpp
@@ -7,14 +7,14 @@
 class Mocha : public CondimentDecorator
 {
 public:
-    explicit Mocha(std::unique_ptr<Beverage> beverage);
+    explicit Mocha(std::unique_ptr<Coffe> coffe);
     ~Mocha() override = default;
 
     std::string getDescription() const override;
     double cost() const override;
 
 private:
-    std::unique_ptr<Beverage> beverage;
+    std::unique_ptr<Coffe> coffe;
 };
 
 #endif // MOCHA_HPP
